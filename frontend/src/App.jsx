5f54f3ae227c49dc9c91dcd003c23db939d4a839
MariamSawera <mariamsawera@gmail.com>
2026-01-04 23:42:08 +0500

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route, Routes} from "react-router"
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/ element={<HomePage>}"></Route>
+    </div>
+  )
+}
+
+export default App
